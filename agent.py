90caa932509e17d9fa8a0da91d338852a99a6579
muhammad Ashiq <muhammadashiqmagsii@gmail.com>
2026-09-23 21:56:06 +0500

    Update agent.py

--- agent.py
+++ agent.py
@@ -1,15 +1,25 @@
+import os
 import streamlit as st
 from crewai import Agent, LLM
 
 
 def create_study_tutor():
 
+    # Get Groq API key from Streamlit Secrets
+    groq_api_key = st.secrets["GROQ_API_KEY"]
+
+    # Give the key to CrewAI
+    os.environ["GROQ_API_KEY"] = groq_api_key
+
+    # Create Groq LLM
     groq_llm = LLM(
         model="groq/openai/gpt-oss-20b",
-        api_key=st.secrets["GROQ_API_KEY"],
-        base_url="https://api.groq.com/openai/v1"
+        api_key=groq_api_key,
+        base_url="https://api.groq.com/openai/v1",
+        temperature=0.3
     )
 
+    # Create Study Tutor Agent
     tutor = Agent(
         role="Study Tutor",
 
@@ -23,6 +33,8 @@ def create_study_tutor():
         """,
 
         llm=groq_llm,
+
+        allow_delegation=False,
 
         verbose=True
     )
