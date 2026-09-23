28569914a210c7817060b4bdd521849c95df0243
muhammad Ashiq <muhammadashiqmagsii@gmail.com>
2026-09-23 21:52:32 +0500

    Update agent.py

--- agent.py
+++ agent.py
@@ -1,19 +1,29 @@
-from crewai import Agent
+import streamlit as st
+from crewai import Agent, LLM
 
 
 def create_study_tutor():
 
+    groq_llm = LLM(
+        model="groq/openai/gpt-oss-20b",
+        api_key=st.secrets["GROQ_API_KEY"],
+        base_url="https://api.groq.com/openai/v1"
+    )
+
     tutor = Agent(
         role="Study Tutor",
-        
+
         goal="Help students understand their study topics clearly.",
-        
+
         backstory="""
         You are a friendly and patient study tutor.
-        You explain difficult topics in simple language.
-        You use simple examples and help students learn step by step.
+        You explain difficult topics in very simple language.
+        You use simple examples.
+        You teach students step by step.
         """,
-        
+
+        llm=groq_llm,
+
         verbose=True
     )
 
